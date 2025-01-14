86fa56efaaa02b86d393abd5499fe3f942a9edb2
Crypto-Grizzly <113410241+Crypto-Grizzly@users.noreply.github.com>
2025-01-14 16:28:36 -0600

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,9 +1,14 @@
 from web3 import Web3
+import itertools
 
 # Настройки
-RPC_URL = 'https:RPC'  # Укажите ваш RPC URL
+RPC_URLS = [
+    'https://arbitrum.llamarpc.com/',
+    'https://arb1.arbitrum.io/rpc',
+    'https://rpc.ankr.com/arbitrum'
+]  # Укажите ваш RPC URL
 CONTRACT_ADDRESS = '0xcontract'  # Укажите адрес контракта
-MINT_GROUPS = list(range(0, 2))  # Группы для минта, укажите диапазон
+MINT_GROUPS = list(range(0, 3))  # Группы для минта, укажите диапазон
 WALLETS_FILE = 'wallets.txt'  # Файл с кошельками
 
 # ABI контракта (замените на актуальное значение)
@@ -39,24 +44,43 @@ def load_wallets(filename, w3):
     checksum_wallets = [w3.to_checksum_address(wallet) for wallet in wallets]
     return checksum_wallets
 
+# Получение следующего RPC из списка (циклически)
+def get_next_rpc():
+    for rpc in itertools.cycle(RPC_URLS):
+        yield rpc
+
 # Функция для проверки mintQuotas
 def check_mint_eligibility(w3, contract, group, wallet_address):
     return contract.functions.mintQuotas(group, wallet_address).call()
 
 # Основная функция
 def main():
-    w3 = Web3(Web3.HTTPProvider(RPC_URL))
-    if not check_rpc_connection(w3):
-        return
+    rpc_generator = get_next_rpc()
+    wallets = []
+    contract = None
 
-    wallets = load_wallets(WALLETS_FILE, w3)
-    contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=contract_abi)
+    for rpc in rpc_generator:
+        try:
+            w3 = Web3(Web3.HTTPProvider(rpc))
+            if check_rpc_connection(w3):
+                wallets = load_wallets(WALLETS_FILE, w3)
+                contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=contract_abi)
+                break
+        except Exception as e:
+            print(f"Ошибка подключения к RPC {rpc}: {e}")
+
+    if not wallets or not contract:
+        print("Не удалось установить соединение с RPC-серверами.")
+        return
 
     print("Результаты проверки:\n")
     for wallet in wallets:
         eligible = False
         for group in MINT_GROUPS:
             try:
+                rpc = next(rpc_generator)
+                w3 = Web3(Web3.HTTPProvider(rpc))
+                contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=contract_abi)
                 quota = check_mint_eligibility(w3, contract, group, wallet)
                 if quota > 0:
                     print(f"Кошелек: {wallet} | Статус: Eligible | Группа: {group} | Quota: {quota}")
